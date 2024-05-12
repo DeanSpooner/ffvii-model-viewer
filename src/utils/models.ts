e51d9e58428a7724d9ba300d88c2066a2b468845
Dean Spooner <dean.spooner@me.com>
2024-05-12 12:22:19 +0100

    More import changes.

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -3,20 +3,20 @@ import { Character } from "./characters";
 export const getGlb = (character: Character) => {
   switch (character) {
     case "aerith":
-      return "public/models/aerith/aerith.glb";
+      return "models/aerith/aerith.glb";
     case "barret":
-      return "public/models/barret/barret.glb";
+      return "models/barret/barret.glb";
     case "caitsith":
-      return "public/models/caitsith/caitsith.glb";
+      return "models/caitsith/caitsith.glb";
     case "cloud":
-      return "public/models/cloud/cloud.glb";
+      return "models/cloud/cloud.glb";
     case "red13":
-      return "public/models/red13/red13.glb";
+      return "models/red13/red13.glb";
     case "sephiroth":
-      return "public/models/sephiroth/sephiroth.glb";
+      return "models/sephiroth/sephiroth.glb";
     case "tifa":
-      return "public/models/tifa/tifa.glb";
+      return "models/tifa/tifa.glb";
     case "vincent":
-      return "public/models/vincent/vincent.glb";
+      return "models/vincent/vincent.glb";
   }
 };
